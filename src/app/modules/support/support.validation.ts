558315007f727844672e23377e60cfee28117bbf
Md Mohosin <mohosinali075@gmail.com>
2025-12-08 23:24:02 +0600

    set priority

--- src/app/modules/support/support.validation.ts
+++ src/app/modules/support/support.validation.ts
@@ -1,29 +1,34 @@
-import { z } from 'zod';
-import { SUPPORT_STATUS } from '../../../enum/support';
+import { z } from 'zod'
+import { SUPPORT_STATUS } from '../../../enum/support'
 
 export const createSupportSchema = z.object({
-  body : z.object({
-  userId: z.string().optional(),
-  subject: z.string().optional(),
-  message: z.string(),
-  status: z.enum([
-    SUPPORT_STATUS.DELETED,
-    SUPPORT_STATUS.IN_PROGRESS,
-    SUPPORT_STATUS.SOLVED,
-  ]).optional(),
-  attachments: z.array(z.string()).optional(),
-  })
-});
+  body: z.object({
+    userId: z.string().optional(),
+    subject: z.string().optional(),
+    message: z.string(),
+    status: z
+      .enum([
+        SUPPORT_STATUS.DELETED,
+        SUPPORT_STATUS.IN_PROGRESS,
+        SUPPORT_STATUS.SOLVED,
+      ])
+      .optional(),
+    priority: z.enum(['low', 'medium', 'high']).optional(),
+    attachments: z.array(z.string()).optional(),
+  }),
+})
 
 export const updateSupportSchema = z.object({
   body: z.object({
     subject: z.string().optional(),
     message: z.string().optional(),
-    status: z.enum([
-      SUPPORT_STATUS.DELETED,
-      SUPPORT_STATUS.IN_PROGRESS,
-      SUPPORT_STATUS.SOLVED,
-    ]).optional(),
+    status: z
+      .enum([
+        SUPPORT_STATUS.DELETED,
+        SUPPORT_STATUS.IN_PROGRESS,
+        SUPPORT_STATUS.SOLVED,
+      ])
+      .optional(),
     attachments: z.array(z.string()).optional(),
   }),
-});
+})
