58637b3ad6564fca1a60e4351526b3b707baad0f
Md Mohosin <mohosinali075@gmail.com>
2026-03-04 03:02:09 +0600

    fix(pagination): change default limit from 10 to 20

--- src/helpers/paginationHelper.ts
+++ src/helpers/paginationHelper.ts
@@ -2,7 +2,7 @@ import { IPaginationOptions } from '../interfaces/pagination'
 
 const calculatePagination = (options: IPaginationOptions) => {
   const page = Number(options.page || 1)
-  const limit = Number(options.limit || 10)
+  const limit = Number(options.limit || 20)
 
   const skip = (page - 1) * limit
 
